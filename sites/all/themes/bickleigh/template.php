903d8721e1b4439af7ff1ddfb1bfec764b84215e
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-27 15:36:08 +0100

    various css and layout tweaks

--- sites/all/themes/bickleigh/template.php
+++ sites/all/themes/bickleigh/template.php
@@ -1,4 +1,11 @@
 <?php
+function bickleigh_css_alter(&$css) {
+  unset($css['modules/system/system.base.css']);
+  unset($css['modules/system/system.menus.css']);
+  unset($css['modules/system/system.messages.css']);
+  unset($css['modules/system/system.theme.css']);
+  unset($css['sites/all/modules/civicrm/css/civicrm.css']);
+}
 
 function bickleigh_form_user_login_block_alter(&$form, &$form_state, $form_id){
 
@@ -15,7 +22,7 @@ function bickleigh_form_user_login_block_alter(&$form, &$form_state, $form_id){
   $form['group']['actions']['submit']['#attributes']['class'][] = 'btn btn-default navbar-btn';
 
 
-  $form['links']['#markup'] = '<p><a href="/user/register">Register</a> | <a href="user/password">Forgot your password?</a></p>';
+  $form['links']['#markup'] = '<p class="login-links"><a href="/user/register">Register</a> | <a href="user/password">Forgot your password?</a></p>';
   $form['links']['#weight'] = 1;
 
   unset($form['name']);
--- sites/all/themes/bickleigh/templates/page.tpl.php
+++ sites/all/themes/bickleigh/templates/page.tpl.php
@@ -23,12 +23,14 @@
   </div>
 </nav>
 <div class="container">
+
   <?php print render($title_prefix); ?>
   <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
   <?php print render($title_suffix); ?>
-  <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+  <?php if (isset($tabs['primary']) && $tabs['primary'] || isset($tabs['secondary']) && $tabs['secondary']): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
   <?php print render($page['help']); ?>
   <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+  <?php if ($messages): ?><div class="alert alert-info" role="alert"><?php print $messages; ?></div><?php endif; ?>
   <?php print render($page['content']); ?>
   <?php print $feed_icons; ?>
 </div>
