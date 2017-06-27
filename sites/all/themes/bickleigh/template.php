6f0fb013a44790d955b75cd15b872e2efb61ea29
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-27 09:31:51 +0100

    proper links for login form

--- sites/all/themes/bickleigh/template.php
+++ sites/all/themes/bickleigh/template.php
@@ -15,12 +15,12 @@ function bickleigh_form_user_login_block_alter(&$form, &$form_state, $form_id){
   $form['group']['actions']['submit']['#attributes']['class'][] = 'btn btn-default navbar-btn';
 
 
-  $form['links']['#markup'] = '<p><a href="/register">Register</a> | <a href="/register">Forgot your password?</a></p>';
+  $form['links']['#markup'] = '<p><a href="/user/register">Register</a> | <a href="user/password">Forgot your password?</a></p>';
   $form['links']['#weight'] = 1;
 
   unset($form['name']);
   unset($form['pass']);
   unset($form['actions']);
 
-  
+
 }
