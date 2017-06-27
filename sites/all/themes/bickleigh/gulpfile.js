62247c5a16fef383c085ab4d25c20061b4c3ce50
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-27 15:33:37 +0100

    adding plumber so watch does not die on less errors

--- sites/all/themes/bickleigh/gulpfile.js
+++ sites/all/themes/bickleigh/gulpfile.js
@@ -1,24 +1,27 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+
 
 gulp.task('default', [ 'less' ]);
 
 gulp.task('less', function(){
-  return gulp.src('less/')
-  .pipe(less())
-  .pipe(gulp.dest('css/'));
+  gulp.src('less/')
+    .pipe(less())
+    .pipe(gulp.dest('css/'));
 });
 
 gulp.task('watch', function(){
-  return watch('less/**', ['less'])
-  .pipe(less())
-  .pipe(gulp.dest('css/'));
+  watch('less/**', ['less'])
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest('css/'));
 });
 
 gulp.task('build', function() {
-   gulp.src('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')
-   .pipe(gulp.dest('css'));
-   gulp.src('vendor/fortawesome/font-awesome/**')
-   .pipe(gulp.dest('css/font-awesome'));
+  gulp.src('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest('css'));
+  gulp.src('vendor/fortawesome/font-awesome/**')
+    .pipe(gulp.dest('css/font-awesome'));
 });
