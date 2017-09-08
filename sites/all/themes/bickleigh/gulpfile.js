b8ae9c27d9f748a032919fd881523dd09dc30293
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-09-08 20:17:23 +0100

    wrking gulpfile

--- sites/all/themes/bickleigh/gulpfile.js
+++ sites/all/themes/bickleigh/gulpfile.js
@@ -3,25 +3,16 @@ var less = require('gulp-less');
 var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
 
-
-gulp.task('default', [ 'less' ]);
-
-gulp.task('less', function(){
-  gulp.src('less/')
-    .pipe(less())
-    .pipe(gulp.dest('css/'));
-});
-
-gulp.task('watch', function(){
-  watch('less/**', ['less'])
+gulp.task('styles', function () {
+  return gulp.src('less/*.less')
+    .pipe(watch('less/*.less'))
     .pipe(plumber())
     .pipe(less())
-    .pipe(gulp.dest('css/'));
-});
-
-gulp.task('build', function() {
-  gulp.src('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')
     .pipe(gulp.dest('css'));
-  gulp.src('vendor/fortawesome/font-awesome/**')
-    .pipe(gulp.dest('css/font-awesome'));
 });
+// gulp.task('build', function() {
+//   gulp.src('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')
+//     .pipe(gulp.dest('css'));
+//   gulp.src('vendor/fortawesome/font-awesome/**')
+//     .pipe(gulp.dest('css/font-awesome'));
+// });
