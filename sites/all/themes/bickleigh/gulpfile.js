67d19173b31f8f753a7020b9d0d1173edbecf2aa
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-09-09 21:42:09 +0100

    new gulpfile

--- sites/all/themes/bickleigh/gulpfile.js
+++ sites/all/themes/bickleigh/gulpfile.js
@@ -3,16 +3,17 @@ var less = require('gulp-less');
 var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
 
-gulp.task('styles', function () {
+gulp.task('watch', function () {
   return gulp.src('less/*.less')
     .pipe(watch('less/*.less'))
     .pipe(plumber())
     .pipe(less())
     .pipe(gulp.dest('css'));
 });
-// gulp.task('build', function() {
-//   gulp.src('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')
-//     .pipe(gulp.dest('css'));
-//   gulp.src('vendor/fortawesome/font-awesome/**')
-//     .pipe(gulp.dest('css/font-awesome'));
-// });
+
+gulp.task('build', function() {
+  gulp.src('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest('css'));
+  gulp.src('vendor/fortawesome/font-awesome/**')
+    .pipe(gulp.dest('css/font-awesome'));
+});
